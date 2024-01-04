d758529e249b506190b78263c8dfaa1fb30dabc2
v47 <victor.verg@hotmail.com>
2024-01-04 14:40:12 +0100

    cert-manager with custom resources

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.25.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.1"
+    }
+  }
+}
